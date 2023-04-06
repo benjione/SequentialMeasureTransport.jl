cc3d1b606807e410f90e5be85ead64882a49fe0c
Benjamin Zanger <benjamin.zanger@inria.fr>
2023-04-06 15:18:40 +0200

    replace tr(A * B) by dot(A', B) for better performance

--- src/TraceModels/models.jl
+++ src/TraceModels/models.jl
@@ -4,10 +4,14 @@ include("polynomial.jl")
 ## trace model evaluation
 function (a::TraceModel{T})(x::PSDdata{T}) where {T<:Number}
     M = ΦΦT(a, x)
-    return tr(a.B * M)
+    # return tr(a.B * M)
+    # tr(B * M) = dot(B', M) = dot(B, M), but faster evaluation
+    return dot(a.B, M)
 end
 
 function (a::TraceModel{T})(x::PSDdata{T}, B::AbstractMatrix{T}) where {T<:Number}
     M = ΦΦT(a, x)
-    return tr(B * M)
+    # return tr(B * M)
+    # tr(B * M) = tr(M * B) = dot(M', B) = dot(M, B) , but faster evaluation
+    return dot(M, B)
 end
