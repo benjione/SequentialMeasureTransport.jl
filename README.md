33b1d1c9d21d6d93795d49c5175fbea32a505d90
Benjamin Zanger <benjamin.zanger@inria.fr>
2024-09-24 13:58:06 +0200

    add docs banner to readme, disable running examples by literated everytime it is pushed

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # SequentialMeasureTransport.jl
+[![Stable](https://img.shields.io/badge/docs-dev-blue.svg)](https://benjione.github.io/SequentialMeasureTransport.jl/dev/)
 
 Code for the paper [Sequential Transport maps using SoS densities and $\alpha$-divergences](https://arxiv.org/abs/2402.17943)
 
-A more detailed documentation is comming soon.
--- docs/make.jl
+++ docs/make.jl
@@ -7,21 +7,23 @@ using Literate
 const EXAMPLES_DIR = joinpath(@__DIR__, "..", "examples")
 const OUTPUT_DIR   = joinpath(@__DIR__, "src/literated")
 
-examples = [
-  "1D_density_estimation_single_map.jl",
-  "1D_density_from_data.jl",
-]
+
+## Run literater if you update an example manually
+# examples = [
+#   "1D_density_estimation_single_map.jl",
+#   "1D_density_from_data.jl",
+# ]
 
 
-for example in examples
-  withenv("GITHUB_REPOSITORY" => "FourierFlows/GeophysicalFlowsDocumentation") do
-    example_filepath = joinpath(EXAMPLES_DIR, example)
-    withenv("JULIA_DEBUG" => "Literate") do
-      Literate.markdown(example_filepath, OUTPUT_DIR;
-                        flavor = Literate.DocumenterFlavor(), execute = true)
-    end
-  end
-end
+# for example in examples
+#   withenv("GITHUB_REPOSITORY" => "FourierFlows/GeophysicalFlowsDocumentation") do
+#     example_filepath = joinpath(EXAMPLES_DIR, example)
+#     withenv("JULIA_DEBUG" => "Literate") do
+#       Literate.markdown(example_filepath, OUTPUT_DIR;
+#                         flavor = Literate.DocumenterFlavor(), execute = true)
+#     end
+#   end
+# end
 
 
 makedocs(sitename="SequentialMeasureTransport.jl",
